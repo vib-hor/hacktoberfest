0e8678ec6556ba542e0c0793c01b199f8cc541e3
Sudutt <harne.sudutt11@gmail.com>
2018-10-04 12:48:11 +0530

    Add lorem ipsum text in code

--- test1.py
+++ test1.py
@@ -1,2 +1,6 @@
-#an empty file to show work on algo, maybe
+#an not so empty file; will be used to show work on algo, maybe
 
+for i in range(20):
+    for j in range(10):
+        print(str(i)+" "+str(j),end="\r")
+    print()
